14c5fe41acb54f3d013cd004fe69e27ed20bdd39
Alex Vertin <avertin@tableau.com>
2020-12-10 17:17:53 -0800

    Repro setup

--- App.js
+++ App.js
@@ -1,12 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import { AutoDragSortableView } from 'react-native-drag-sort'
+import { range } from 'lodash'; 
 
 export default function App() {
+  const [content, setContent] = useState(range(0, 4).map(x => ({ id: x })));
+  const parentWidth = Dimensions.get('window').width;
+  const childrenWidth = (parentWidth / 2) - 50;
+  const childrenHeight = childrenWidth;
+  
+  const renderItem = (item, index) => {
+    return ( 
+      <View style={[{ height: childrenHeight, width: childrenWidth }, styles.item]}>
+        <Text style={{ fontSize: 40 }}>{item.id}</Text>
+      </View>
+    );
+  }
+
+  const handleDragEnd = (fromIndex, toIndex) => {
+    // update the content order 
+    console.log('handleDragEnd - fromIndex', fromIndex);
+    console.log('handleDragEnd - toIndex', toIndex);
+    let updatedContent = [...content];
+    updatedContent.splice(toIndex, 0, updatedContent.splice(fromIndex, 1)[0]);
+    console.log('handleDragEnd - updatedContent', updatedContent);
+    setContent(updatedContent)
+  }
+
+  const handleDataChange = (data) => {
+    console.log('handleDataChange - data', data);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <AutoDragSortableView
+        dataSource={content}
+        parentWidth={parentWidth}
+        childrenWidth= {childrenWidth}
+        childrenHeight={childrenHeight}
+        keyExtractor={(item,index)=> item.id}
+        renderItem={renderItem}
+        onDragEnd={handleDragEnd}
+        onDataChange={handleDataChange}
+      />
     </View>
   );
 }
@@ -17,5 +53,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 50
   },
+  item: {
+    backgroundColor: 'grey', 
+    alignContent: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'white',
+    borderWidth: 3
+  }
 });
